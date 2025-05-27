f4071e39624d910b1344a637ada5b59f3a5d7e3e
vivitha1703 <vivitha17m@gmail.com>
2025-05-27 18:05:42 +0530

    Hashed the password

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
     host: 'localhost',
-    user: 'root', // replace with your MySQL username
-    password: 'Vivitha@17', // replace with your MySQL password
+    user: 'root',  // Replace with MySQL root username
+    password: '#######', // Replace with  MySQL root password
     database: 'todo_app',
     waitForConnections: true,
     connectionLimit: 10,
